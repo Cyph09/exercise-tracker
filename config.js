613203640ceadc93f6abacab2c9f2fb4aea99875
Swaleh Mhina <cyph09@gmail.com>
2019-11-16 04:52:42 +0300

    Added mongo atlas URI

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    ENV: process.env.NODE_ENV || "development",
-    PORT: process.env.PORT || 5000,
-    MONGODB_URI:
-      process.env.MONGODB_URI ||
-    "mongodb://mhina:mhina123@localhost/exercise"
-  };
-  
+  ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || 5000,
+  MONGODB_URI:
+    process.env.MONGODB_URI ||
+    "mongodb+srv://mhina:mhina1234@exercise-tracker-fh8jy.mongodb.net/test?retryWrites=true&w=majority"
+  // process.env.MONGODB_URI || "mongodb://mhina:mhina123@localhost/exercise"
+};
